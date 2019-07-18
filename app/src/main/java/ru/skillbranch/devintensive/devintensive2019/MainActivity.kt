d571a25603fc7f6347a3e483d9ea0914090f50b2
savelgmx <savel@gmx.com>
2019-07-18 15:04:57 +0700

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/devintensive2019/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.devintensive2019
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
