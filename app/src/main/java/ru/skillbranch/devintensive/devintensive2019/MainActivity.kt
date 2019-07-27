c27a8eafaeaa15cd0423389d2ae4bc092e7ab2f9
savelgmx <savel@gmx.com>
2019-07-27 20:00:52 +0700

    Commit #4 hometask_3 layput change

--- app/src/main/java/ru/skillbranch/devintensive/devintensive2019/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/devintensive2019/MainActivity.kt
@@ -1,12 +1,14 @@
 package ru.skillbranch.devintensive.devintensive2019
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("M_MainActivity","onCreate")
     }
 }
